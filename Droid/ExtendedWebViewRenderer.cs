8c7266dc74eb3c47103f8cc2b96c95ebe27d4fd4
Sharada Gururaj <ms.sharada@gmail.com>
2017-08-06 20:32:12 -0400

    Added sample for android and iOS

---
+++ Droid/ExtendedWebViewRenderer.cs
@@ -0,0 +1,65 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+using Android;
+using Android.Content;
+using Android.Webkit;
+using Android.App;
+
+using LocationWebView;
+using LocationWebView.Droid;
+
+[assembly: ExportRenderer(typeof(ExtendedWebView), typeof(ExtendedWebViewRenderer))]
+namespace LocationWebView.Droid
+{
+    public class ExtendedWebViewRenderer : WebViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.WebView> e)
+        {
+            base.OnElementChanged(e);
+
+			if (e.OldElement != null || Element == null)
+			{
+				return;
+			}
+
+            Control.Settings.JavaScriptEnabled = true;
+			Control.Settings.SetGeolocationEnabled(true);
+            Control.Settings.SetGeolocationDatabasePath(Control.Context.FilesDir.Path);
+
+			Control.SetWebViewClient(new CustomWebViewClient());
+			Control.SetWebChromeClient(new CustomChromeClient(Control.Context));
+        }
+    }
+
+	public class CustomWebViewClient : WebViewClient
+    {
+		public override bool ShouldOverrideUrlLoading(Android.Webkit.WebView view, string url)
+        {
+			view.LoadUrl(url);
+			return true;
+        }
+	}
+
+	public class CustomChromeClient : WebChromeClient
+    {
+		private readonly Context _context;
+
+		public CustomChromeClient(Context context)
+		{
+			_context = context;
+		}
+
+		public override void OnGeolocationPermissionsShowPrompt(string origin, GeolocationPermissions.ICallback callback)
+		{
+			const bool remember = false;
+			var builder = new AlertDialog.Builder(_context);
+			builder.SetTitle("Location")
+				.SetMessage(string.Format("{0} would like to use your current location", origin))
+				.SetPositiveButton("Allow", (sender, args) => callback.Invoke(origin, true, remember))
+				.SetNegativeButton("Disallow", (sender, args) => callback.Invoke(origin, false, remember));
+			var alert = builder.Create();
+			alert.Show();
+		}
+	}
+}
---
+++ LocationWebView/LocationWebViewPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace LocationWebView
+{
+    public partial class LocationWebViewPage : ContentPage
+    {
+        public LocationWebViewPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
